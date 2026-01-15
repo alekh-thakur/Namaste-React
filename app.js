8149c78865ba812d67bb990d3d849636b80af19b
alekh-thakur <alekhrathor8@gmail.com>
2026-01-15 21:45:02 +0530

    Igniting

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const parent = React.createElement("div",{id:"head"},[ 
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Hello from nested tag"),
-        React.createElement("h2",{},"Hello from nested tag")]
-    ),React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"Hello from nested tag"),
-        React.createElement("h2",{},"Hello from nested tag")]
-    )])
+import React from "react";
+import {createRoot} from "react-dom/client";
+const parent = React.createElement("div", {id:"head"}, [ 
+    React.createElement("div", {id:"child1"}, [
+        React.createElement("h1", {}, "this is namaste react"),
+        React.createElement("h2", {}, "this is namaste react")]
+    ),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {}, "hello"),
+        React.createElement("h2", {}, "ggggg")]
+    )
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+const root = createRoot(document.getElementById("root"));
+root.render(parent);
